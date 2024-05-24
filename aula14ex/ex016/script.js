80431cefcb2199b133687326ff1871444caba89a
Yuli Gonçalves <162021541+yuligoncalves@users.noreply.github.com>
2024-05-24 15:30:36 -0300

    terminei exercício 16

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,25 +1,31 @@
 function contar() {
-    var inicio = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passo = document.getElementById('pula')
-    var res = document.getElementById('res')
+    let inicio = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('pula')
+    let res = document.getElementById('res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || pula.value.length == 0) {
         res.innerHTML = `Impossível contar!`
     } else {
-        res.innerHTML = `Contando:`
-        var i = Number(inicio.value)
-        var f = Number(fim.value)
-        var p = Number(pula.value)
+        res.innerHTML = `Contando: `
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(pula.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando passo 1')
+            p = 1
+        }  
         if (i < f) {
-            for(var c = i; c <= f; c += p) {
-                res.innerHTML += ` ${c} \u{IF449}`
+            for(let c = i; c <= f; c+= p) {
+                res.innerHTML += `${c} \u{1F449}`
             }
         } else {
-            for(var c = i; c >= f; c -= p) {
-                res.innerHTML += `${c} \u{IF449}`
-            }
+            // contagem regressiva
+            for(let c = i; c>= f; c-= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            } 
         }
-       
+        res.innerHTML += `\u{1F3C1}`
+
     }
 }
