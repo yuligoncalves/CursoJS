722871ecfe99b96e88afc96213deb610c142fca0
Yuli Gonçalves <162021541+yuligoncalves@users.noreply.github.com>
2024-05-26 18:05:02 -0300

    testando funções

---
+++ funcao01.js
@@ -0,0 +1,11 @@
+function parimp(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+console.log(parimp(40))
+/*let res = parimp(11)
+console.log(res)
+*/
